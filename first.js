706968186f5a63494bce4f395e736174281ae36a
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-03-14 14:01:16 +0430

    Second Commit

--- first.js
+++ first.js
@@ -1,8 +1,4 @@
-let q1 = document.querySelector('.question1');
-let q2 = document.querySelector('.question2');
-let q3 = document.querySelector('.question3');
-let q4 = document.querySelector('.question4');
-let q5 = document.querySelector('.question5');
+
 let form = document.querySelector('.qform');
 
 
@@ -11,6 +7,6 @@ const answers = ['Tehran','Kabul','Pakistan',32,'Hyper Text Markup Language'];
 
 form.addEventListener('submit', e =>{
     e.preventDefault();
-    console.log(form.answer1);
+    console.log("Form Submited");
 
 });
