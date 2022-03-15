da11edeaee947f9f1a2b92433fad28cae97d880e
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-03-15 10:26:19 +0430

    changed style and script files

--- first.js
+++ first.js
@@ -2,19 +2,30 @@
 
 let form = document.querySelector('.qform');
 let resulttitle = document.querySelector('.result-title1');
-const answers = ['Tehran','Kabul','Pakistan',50,'Hyper Text Markup Language'];
+const answers = ['tehran','kabul','pakistan',"50",'hyper text markup language'];
+let test = 5; 
+
  form.addEventListener('submit',e =>{
     e.preventDefault();
     let selected = document.querySelectorAll('input[type="radio"]:checked');
-    selected.forEach(value =>{
-            let result = " ";
-            result += value.parentElement.textContent; 
-
-            resulttitle.textContent += result + " ";
-            
 
 
-    });
+        let counter = 0; 
+        selected.forEach((value, index) =>{
+
+                        if(answers[index] === value.value){
+
+                                counter++; 
+                                
+                        }else{
+                                counter = counter;
+                                
+                        }
+
+        });
+        console.log(counter);
+
+
 
  });
 
