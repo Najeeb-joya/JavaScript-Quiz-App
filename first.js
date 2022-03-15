c54f04e01adaae103e621399391f6a934d83af1d
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-03-15 11:47:21 +0430

    final changes

--- first.js
+++ first.js
@@ -22,8 +22,27 @@ const answers = ['tehran','kabul','pakistan',"50",'hyper text markup language'];
                         }
 
         });
+        if(counter > 2){
+        title.setAttribute('class','success');
         title.textContent = "Congrats! You Compeleted the Test & Below is Your Result "
         resulttitle.textContent = "You Scored " + counter + "/5"
+        }else{
+                title.setAttribute('class','error');
+                title.textContent = "Oops! You Faild Try Agrain"
+                resulttitle.textContent = "You Scored " + counter + "/5"
+        }
+
+        window.scroll({
+                top: 0, 
+                left: 0, 
+                behavior: 'smooth'
+        });
+
+        // window.scroll({
+        //         top: 0, 
+        //         left: 0, 
+        //         behavior: 'smooth' 
+        //        });
 
 
 
