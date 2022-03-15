0ea9d28a5cd3d37aeebc8ab737301e6ca83ea9ac
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-03-15 10:52:38 +0430

    final changes for today

--- first.js
+++ first.js
@@ -1,14 +1,13 @@
 
 
 let form = document.querySelector('.qform');
+let title = document.querySelector('.result-title');
 let resulttitle = document.querySelector('.result-title1');
 const answers = ['tehran','kabul','pakistan',"50",'hyper text markup language'];
-let test = 5; 
 
  form.addEventListener('submit',e =>{
     e.preventDefault();
     let selected = document.querySelectorAll('input[type="radio"]:checked');
-
 
         let counter = 0; 
         selected.forEach((value, index) =>{
@@ -23,7 +22,8 @@ let test = 5;
                         }
 
         });
-        console.log(counter);
+        title.textContent = "Congrats! You Compeleted the Test & Below is Your Result "
+        resulttitle.textContent = "You Scored " + counter + "/5"
 
 
 
