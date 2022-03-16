e86fb65d20b720277ebe382baae27ed385def107
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-03-16 13:54:44 +0430

    Some changes

--- first.js
+++ first.js
@@ -5,31 +5,32 @@ let title = document.querySelector('.result-title');
 let resulttitle = document.querySelector('.result-title1');
 const answers = ['tehran','kabul','pakistan',"50",'hyper text markup language'];
 
+
  form.addEventListener('submit',e =>{
-    e.preventDefault();
+   e.preventDefault();
+    
     let selected = document.querySelectorAll('input[type="radio"]:checked');
+    console.log(selected);
 
         let counter = 0; 
         selected.forEach((value, index) =>{
 
                         if(answers[index] === value.value){
 
-                                counter++; 
-                                
+                                counter +=20; 
                         }else{
                                 counter = counter;
                                 
                         }
-
         });
         if(counter > 2){
         title.setAttribute('class','success');
         title.textContent = "Congrats! You Compeleted the Test & Below is Your Result "
-        resulttitle.textContent = "You Scored " + counter + "/5"
+        resulttitle.textContent = "You Scored " + counter + "%"
         }else{
                 title.setAttribute('class','error');
                 title.textContent = "Oops! You Faild Try Agrain"
-                resulttitle.textContent = "You Scored " + counter + "/5"
+                resulttitle.textContent = "You Scored " + counter + "%"
         }
 
         window.scroll({
@@ -38,12 +39,8 @@ const answers = ['tehran','kabul','pakistan',"50",'hyper text markup language'];
                 behavior: 'smooth'
         });
 
-        // window.scroll({
-        //         top: 0, 
-        //         left: 0, 
-        //         behavior: 'smooth' 
-        //        });
 
+      
 
 
  });
