3777d92121ac34526f926cc77c7e5630ab0fa7e6
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-03-17 14:25:05 +0430

    show result by percentage added

--- first.js
+++ first.js
@@ -4,6 +4,7 @@ let form = document.querySelector('.qform');
 let title = document.querySelector('.result-title');
 let resulttitle = document.querySelector('.result-title1');
 const answers = ['tehran','kabul','pakistan',"50",'hyper text markup language'];
+let counter = 0; 
 
 
  form.addEventListener('submit',e =>{
@@ -11,7 +12,7 @@ const answers = ['tehran','kabul','pakistan',"50",'hyper text markup language'];
     
     let selected = document.querySelectorAll('input[type="radio"]:checked');
 
-        let counter = 0; 
+        
         selected.forEach((value, index) =>{
 
                 if(answers[index] === value.value){
@@ -21,40 +22,49 @@ const answers = ['tehran','kabul','pakistan',"50",'hyper text markup language'];
                 }
         });
         
-        window.scroll({
-                top: 0, 
-                left: 0, 
-                behavior: 'smooth'
-        });
 
         let score = 1; 
-        
+        let flag="test"
         const intr = setInterval(() => {
+                title.textContent = "Counting Your Score... ";
                if(score === counter){
                        clearInterval(intr);
                }else{
                 score++;
-                title.textContent = "Counting Your Scores";
-                resulttitle.textContent = "You Scored " + score + "%";
-               }
-       },30);
-      
-       console.log(counter);
-       if(counter >= 55){ 
-                title.setAttribute('class','success');
-                title.textContent = "Congrats! You Compeleted the Test & Below is Your Result ";
-                resulttitle.textContent = "You Scored " + counter + "%";
-                console.log("Something for Test");
-        }else{
-                title.setAttribute('class','error');
-                title.textContent = "Oops! You Faild Try Agrain";
-                resulttitle.textContent = "You Scored " + counter + "%";
+                if(score >= 60){
+                        flag ="passed"
+                        resulttitle.textContent= score + "%"; 
+                }else{
+                        flag="failed"
+                }
         }
+       },30);
+       console.log(flag);
 
 
- });
+
+       window.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth'
+});   
+        
+  });
+ 
 
    
+
+
+//  if(score >= 55){ 
+//         title.setAttribute('class','success');
+//         title.textContent = "Congrats! You Compeleted the Test & Below is Your Result ";
+//          resulttitle.textContent = "You Scored " + score + "%";
+// }else{
+//         title.setAttribute('class','error');
+//         title.textContent = "Oops! You Faild Try Agrain";
+//          resulttitle.textContent = "You Scored " + score + "%";
+
+// }
 
 
 
